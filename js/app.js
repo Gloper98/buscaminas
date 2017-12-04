9c36866642c041de9f26d2a4c9cf3ec6f140f2aa
Gloper98 <littleannygutlop_2014@outlook.es>
2017-12-04 01:04:51 -0500

    Updating changes

---
+++ js/app.js
@@ -0,0 +1,77 @@
+var tableBombIt = [
+	               ' ',
+	               '*',
+	               '1',
+	               ' ',
+	               '/n',
+	               ' ',
+	               ' ',
+	               '*',
+	               '2',
+	               '/n',
+	               '1',
+	               '*',
+	               ' ',
+	               ' ',
+	               '/n',
+	               ' ',
+	               '3',
+	               '*',
+	               ' '
+                  ];
+
+var containerBombIt = document.getElementById("container-bomb-it");
+var bombIt = document.getElementById("bomb-it");
+
+var drawGameZone = function(array) {
+	for( var i =0; i< tableBombIt.length; i++){
+		var cell = document.createElement("div");
+		var enter = document.createElement("br");
+		
+		if(array[i] == '1' || array[i] == '2' || array[i] == '3'){
+			cell.className = "number";
+			cell.innerText = array[i];
+			cell.onclick = showNumber;
+		} else if (array[i] == ' '){
+			cell.className = "emptySpace";
+			cell.onclick = changeColor;
+		} else if (array[i] == '*'){
+			cell.className = "bomb";
+			cell.onclick = boom;
+		} else if (array[i] == '/n') {
+			bombIt.appendChild(enter);
+			continue;
+		}
+		
+		bombIt.appendChild(cell);
+	}
+	
+	addButtonRestart();
+};
+
+var boom = function() {
+	containerBombIt.removeChild(bombIt);
+	containerBombIt.innerHTML = "<img src='assets/images/boom.gif' alt='Boom'>"
+	addButtonRestart();
+};
+
+var addButtonRestart = function () {
+	var btnRestart = document.createElement('button');
+	btnRestart.innerText = 'Restart!!';
+	btnRestart.addEventListener('click', reStart);
+	containerBombIt.appendChild(btnRestart);
+}
+
+var showNumber = function() {
+	this.className = "number numberShow";
+};
+
+var changeColor = function() {
+	this.className = "change-color";
+}
+
+var reStart = function() {
+	location.reload();
+}
+
+drawGameZone(tableBombIt);
